bbbf56d6931ae89ab11df43f0a3ac4c74af2ba59
Abrahan Silverio <abrahansmg@gmail.com>
2019-04-12 05:45:47 -0400

    added datepicker and select styles

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,29 @@
 
   // On Document Ready
   $(function (){
-    console.log('Script')
+
+    initSelectpicker()
+
+    // date pickers
+    $('#from-date').datepicker({
+      container:	'.from-date'
+    });
+
+    $('#to-date').datepicker({
+      container:	'.to-date'
+    });
+
   })
+
+  function initSelectpicker() {
+    if ( ! $.fn.selectpicker) return
+
+    //Select Styler
+    $('.select-filter').selectpicker({
+      style: 'form-control',
+      size: 4,
+      title: 'seleccione'
+    })
+  }
 
 })(jQuery)
